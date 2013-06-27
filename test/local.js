7b86b998932c4b50f7b10a30f589ecf935877f2e
kazuya kawaguchi <kawakazu80@gmail.com>
2013-06-27 18:35:19 +0900

    Refactor

--- test/local.js
+++ test/local.js
@@ -16,142 +16,208 @@ var format = require('util').format;
 
 describe('Local()', function () {
 
-  var describeWithSuccessfulCmd = function (cmd) {
-    describe(format('with successful cmd: "%s"', cmd), function () {
-      var local, err, stdout, stderr;
+  describe('#run(cmd, [opts], cb)', function () {
+    describe('with successful cmd: "uname -s"', function () {
       before(function (done) {
-        local = new Local();
-        local.run(cmd, function () {
+        this.local = new Local();
+        this.local.run('uname -s', function () {
           var args = arguments;
-          err = args[0];
-          stdout = args[1];
-          stderr = args[2];
+          this.err = args[0];
+          this.stdout = args[1];
+          this.stderr = args[2];
           done();
-        });
+        }.bind(this));
       });
 
       describe('err', function () {
         it('expect to equal null', function (done) {
-          expect(err).to.eql(null);
+          expect(this.err).to.eql(null);
           done();
         });
       });
 
       describe('stdout', function () {
         it('expect to be greater than 0', function (done) {
-          expect(stdout.length).to.be.greaterThan(0);
+          expect(this.stdout.length).to.be.greaterThan(0);
           done();
         });
       });
 
       describe('stderr', function () {
         it('expect to equal ""', function (done) {
-          expect(stderr).to.eql('')
+          expect(this.stderr).to.eql('')
           done();
         });
       });
 
-    });
-  };
+    }); // end of 'with successful cmd: "uname -s"'
 
-  var describeWithFailedCmd = function (cmd) {
-    describe(format('with failed cmd: "%s"', cmd), function () {
-      var local, err, stdout, stderr;
+    describe('with failed cmd: "uname -u"', function () {
       before(function (done) {
-        local = new Local();
-        local.run(cmd, function () {
+        this.local = new Local();
+        this.local.run('uname -u', function () {
           var args = arguments;
-          err = args[0];
-          stdout = args[1];
-          stderr = args[2];
+          this.err = args[0];
+          this.stdout = args[1];
+          this.stderr = args[2];
           done();
-        });
+        }.bind(this));
       });
 
       describe('err', function () {
         it('expect to be an Error', function (done) {
-          expect(err).to.be.an(Error);
+          expect(this.err).to.be.an(Error);
           done();
         });
       });
 
       describe('err.code', function () {
         it('expect not to equal 0', function (done) {
-          expect(err.code).not.to.eql(0);
+          expect(this.err.code).not.to.eql(0);
           done();
         });
       });
 
       describe('err.signal', function () {
         it('expect to equal null', function (done) {
-          expect(err.signal).to.eql(null);
+          expect(this.err.signal).to.eql(null);
           done();
         });
       });
 
       describe('stdout', function () {
         it('expect to equal ""', function (done) {
-          expect(stdout).to.eql('')
+          expect(this.stdout).to.eql('')
           done();
         });
       });
 
       describe('stderr', function () {
         it('expect to be greater than 0', function (done) {
-          expect(stderr.length).to.be.greaterThan(0);
+          expect(this.stderr.length).to.be.greaterThan(0);
           done();
         });
       });
 
-    });
-  };
+    }); // end of 'with failed cmd: "uname -u"'
 
-
-  describe('#run(cmd, [opts], cb)', function () {
-    describeWithSuccessfulCmd('uname -s');
-    describeWithFailedCmd('uname -u');
-
-    describe('execute multiple', function () {
-      describe('first execute', function () {
-        describeWithSuccessfulCmd('uname -s');
-        describe('second execute', function () {
-          describeWithFailedCmd('uname -u')
-        });
-      });
-    });
-
-    describe('with opts', function () {
-
-      describe('cwd', function () {
-        var err, stdout, stderr;
-        var cwd = process.cwd() + '/test';
+    describe('multiple cmd', function () {
+      describe('first cmd: "uname -s"', function () {
         before(function (done) {
-          new Local().run('pwd', { cwd: cwd }, function () {
+          this.local = new Local();
+          this.local.run('uname -s', function () {
             var args = arguments;
-            err = args[0];
-            stdout = args[1];
-            stderr = args[2];
+            this.err = args[0];
+            this.stdout = args[1];
+            this.stderr = args[2];
             done();
-          });
+          }.bind(this));
         });
 
         describe('err', function () {
           it('expect to equal null', function (done) {
-            expect(err).to.eql(null);
+            expect(this.err).to.eql(null);
             done();
           });
         });
 
         describe('stdout', function () {
-          it(format('expect to contain "%s"', cwd), function (done) {
-            expect(stdout).to.contain(cwd);
+          it('expect to be greater than 0', function (done) {
+            expect(this.stdout.length).to.be.greaterThan(0);
             done();
           });
         });
 
         describe('stderr', function () {
           it('expect to equal ""', function (done) {
-            expect(stderr).to.eql('')
+            expect(this.stderr).to.eql('')
+            done();
+          });
+        });
+
+        describe('second cmd: "uname -s"', function () {
+          before(function (done) {
+            this.local.run('uname -u', function () {
+              var args = arguments;
+              this.err = args[0];
+              this.stdout = args[1];
+              this.stderr = args[2];
+              done();
+            }.bind(this));
+          });
+
+          describe('err', function () {
+            it('expect to be an Error', function (done) {
+              expect(this.err).to.be.an(Error);
+              done();
+            });
+          });
+
+          describe('err.code', function () {
+            it('expect not to equal 0', function (done) {
+              expect(this.err.code).not.to.eql(0);
+              done();
+            });
+          });
+
+          describe('err.signal', function () {
+            it('expect to equal null', function (done) {
+              expect(this.err.signal).to.eql(null);
+              done();
+            });
+          });
+
+          describe('stdout', function () {
+            it('expect to equal ""', function (done) {
+              expect(this.stdout).to.eql('')
+              done();
+            });
+          });
+
+          describe('stderr', function () {
+            it('expect to be greater than 0', function (done) {
+              expect(this.stderr.length).to.be.greaterThan(0);
+              done();
+            });
+          });
+
+        }); // end of 'second cmd: "uname -u"'
+
+      }); // end of 'first cmd: "uname -s"'
+
+    }); // end of 'multiple cmd'
+
+    describe('with opts', function () {
+
+      describe('cwd', function () {
+        var cwd = process.cwd() + '/test';
+        before(function (done) {
+          new Local().run('pwd', { cwd: cwd }, function () {
+            var args = arguments;
+            this.err = args[0];
+            this.stdout = args[1];
+            this.stderr = args[2];
+            done();
+          }.bind(this));
+        });
+
+        describe('err', function () {
+          it('expect to equal null', function (done) {
+            expect(this.err).to.eql(null);
+            done();
+          });
+        });
+
+        describe('stdout', function () {
+          it(format('expect to contain "%s"', cwd), function (done) {
+            expect(this.stdout).to.contain(cwd);
+            done();
+          });
+        });
+
+        describe('stderr', function () {
+          it('expect to equal ""', function (done) {
+            expect(this.stderr).to.eql('')
             done();
           });
         });
@@ -159,34 +225,33 @@ describe('Local()', function () {
 
       describe('env', function () {
         var env = { HOGE: 'hoge' };
-        var err, stdout, stderr;
         before(function (done) {
           new Local().run('env | grep hoge', { env: env }, function () {
             var args = arguments;
-            err = args[0];
-            stdout = args[1];
-            stderr = args[2];
+            this.err = args[0];
+            this.stdout = args[1];
+            this.stderr = args[2];
             done();
-          });
+          }.bind(this));
         });
 
         describe('err', function () {
           it('expect to equal null', function (done) {
-            expect(err).to.eql(null);
+            expect(this.err).to.eql(null);
             done();
           });
         });
 
         describe('stdout', function () {
           it(format('expect to contain "%s"', env.HOGE), function (done) {
-            expect(stdout).to.contain(env.HOGE);
+            expect(this.stdout).to.contain(env.HOGE);
             done();
           });
         });
 
         describe('stderr', function () {
           it('expect to equal ""', function (done) {
-            expect(stderr).to.eql('')
+            expect(this.stderr).to.eql('')
             done();
           });
         });
